699aafa929da6788b95846c3c35f4ed3bc1dc132
matteobucci <matteo.bucci.1995@gmail.com>
2020-10-30 16:43:02 +0100

    Added first utility methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+export function getDaysFrom1970(time: number): number {
+    return (time / (60 * 60 * 1000 * 24));
+}
+
+
+export function getWeeksFrom1970(time: number): number {
+    let dayNumber = getDaysFrom1970(time);
+    dayNumber += 3; //Prima settimana in assoluto (Lunedì 5 gennaio 1970)
+    return dayNumber / 7;
+}
